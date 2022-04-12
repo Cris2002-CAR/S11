134e5528fb248037575c9641f6be3fec08ad1ad3
JesusGarce22 <jesus02garces@gmail.com>
2022-04-11 19:10:12 -0500

    test

---
+++ test/model/TreeTest.java
@@ -0,0 +1,59 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+public class TreeTest {
+	private Tree x;
+
+	@Test
+	public void setupScenary2() {
+		x=new Tree();
+
+		int key=7;
+		int value=3;
+		int key1=10;
+		int value1=4;
+		int key2=84;
+		int value2=1;
+
+		x.add(key, value);
+		x.add(key1, value1);
+		x.add(key2, value2);
+	}
+	@Test
+	public void setupScenary3() {
+		x=new Tree();
+	}
+
+	@Test
+	public void addTree() {
+		setupScenary3();
+		int key=5;
+		int value=3;
+
+		x.add(key, value);
+		Node y=x.triggerSearch(key);
+
+		assertEquals(y.getKey(), key);
+		assertEquals(y.getValue(), value);
+	}
+
+	@Test
+	public void printTree() {
+		setupScenary2();
+
+		x.triggerInOrder();
+
+	}
+
+	@Test
+	public void delete() {
+		setupScenary2();
+
+		int key=5;
+		int value=3;
+
+		x.add(key, value);
+		x.triggerDelete(key);
+	}
+}
